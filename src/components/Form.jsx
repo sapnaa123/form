3737bd4c576fb15b2ebfad5b3af18a74648c2d99
sapnaa123 <sabiriinmohmuud1@gmail.com>
2024-07-17 09:39:55 +0300

    formka waxan ku daray validetion

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,39 @@
 import { useState } from "react"
 function Form( {isClose, Handle}){
-    
 
+    const[title, settitle] = useState("")
+    const[description , setdescription] = useState ("")
+    const[error, setError] = useState(false)
+
+    //validate the form
+
+    const HandleForm =(event) => {
+        event.preventDefault()
+        if(title == "" && description == ""){
+            setError(true)
+        setTimeout(()=> {
+            setError(false)
+        }, 3000);
+        }
+    }
    
 
 
     return <div style={{display: isClose == false ? "block" : "" }} className="bg-gray-700 h-screen w-full absolute top-0 ">
 
      <div className="flex justify-center p-10">
-        <form className="bg-white sm:w-[400px] w-[300px] sm:h-[450px] h-[420px] p-4">
+        <form className="bg-white sm:w-[400px] w-[300px] sm:h-[420px] h-[420px] p-4">
         <i onClick={Handle} class="fa-solid fa-x sm:ml-[350px] ml-[240px]"></i>
+        {
+            error == true ? <p className="text-red-500">Fadlan soo buxi formka</p> : ""
+        }
             <br />
+            <input value={title} onChange={(event) =>{
+                settitle(event.target.value)} }  
+                className="sm:w-[350px] w-[250px] h-[20px] border-2 border-black p-5 rounded" type="text" placeholder="Enter title" />
             <br />
-            <input className="sm:w-[350px] w-[250px] h-[20px] border-2 border-black p-5 rounded" type="password" placeholder="Enter title" />
-            <br />
-            <textarea className="sm:w-[350px] w-[250px] rounded border-2 border-black mt-3 row={8}" placeholder="Enter title"></textarea>
-            <button className="bg-secondryColor text-white sm:w-[350px] w-[250px] h-[40px] text-3xl mt-10 g">save</button>
+            <textarea value={description} className="sm:w-[350px] w-[250px] rounded border-2 border-black mt-3 row={8}" placeholder="Enter title"></textarea>
+            <button onClick={HandleForm} className="bg-secondryColor text-white sm:w-[350px] w-[250px] h-[40px] text-3xl mt-10 g">save</button>
         </form>
     </div>
        
@@ -23,4 +41,4 @@ function Form( {isClose, Handle}){
     </div>
 }
 
-export default Form
+export default Form 
